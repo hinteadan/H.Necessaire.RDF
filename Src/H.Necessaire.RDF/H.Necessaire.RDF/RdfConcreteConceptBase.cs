2ec07c818ccbae89e1e5a9d1bfc19b5c2a5755f7
Hintee <hintea_dan@yahoo.co.uk>
2023-10-17 13:38:50 +0300

    OD: Modelling

---
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF/RdfConcreteConceptBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.RDF
+{
+    public abstract class RdfConcreteConceptBase<TPayload> : RdfConceptBase<TPayload>
+    {
+        #region Construct
+        readonly RdfPayloadAcquirer<TPayload> payloadAcquirer;
+        protected RdfConcreteConceptBase(Func<Task<TPayload>> payloadAcquirer, ImAnRdfConcept meta) : base(meta)
+        {
+            this.payloadAcquirer = new RdfPayloadAcquirer<TPayload>(payloadAcquirer);
+        }
+        protected RdfConcreteConceptBase(TPayload payload, ImAnRdfConcept meta) : base(meta)
+        {
+            this.payloadAcquirer = new RdfPayloadAcquirer<TPayload>(payload);
+        }
+        #endregion
+
+        public override async Task<OperationResult<TPayload>> AcquirePayload() => await payloadAcquirer.AcquirePayload();
+    }
+}
--- Src/H.Necessaire.RDF/H.Necessaire.RDF/RdfSubject.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF/RdfSubject.cs
@@ -3,22 +3,18 @@ using System.Threading.Tasks;
 
 namespace H.Necessaire.RDF
 {
-    public class RdfSubject<TPayload> : RdfConceptBase<TPayload>
+    public class RdfSubject<TPayload> : RdfConcreteConceptBase<TPayload>
     {
         #region Construct
-        readonly RdfPayloadAcquirer<TPayload> payloadAcquirer;
-        public RdfSubject(Func<Task<TPayload>> payloadAcquirer, ImAnRdfConcept meta) : base(meta)
+        public RdfSubject(Func<Task<TPayload>> payloadAcquirer, ImAnRdfConcept meta) : base(payloadAcquirer, meta)
         {
-            this.payloadAcquirer = new RdfPayloadAcquirer<TPayload>(payloadAcquirer);
         }
-        public RdfSubject(TPayload payload, ImAnRdfConcept meta) : base(meta)
+
+        public RdfSubject(TPayload payload, ImAnRdfConcept meta) : base(payload, meta)
         {
-            this.payloadAcquirer = new RdfPayloadAcquirer<TPayload>(payload);
         }
         #endregion
 
         public override RdfConceptType ConceptType { get; } = RdfConceptType.Subject;
-
-        public override async Task<OperationResult<TPayload>> AcquirePayload() => await payloadAcquirer.AcquirePayload();
     }
 }
