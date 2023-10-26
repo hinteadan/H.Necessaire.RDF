213d350de7df3121454013c413ee7e71ecedb1ba
Hintee <hintea_dan@yahoo.co.uk>
2023-10-26 17:42:16 +0300

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/BindOps.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/BindOps.cs
@@ -4,16 +4,16 @@ namespace H.Necessaire.RDF.UI.WindowsDesktop
 {
     static class BindOps
     {
-        public static double Multiply(int unit, double factor) => unit * factor;
+        public static double Size(int unit, double factor) => unit * factor;
 
-        public static GridLength MultiplyForGrid(int unit, double factor) => new GridLength(unit * factor, GridUnitType.Pixel);
+        public static GridLength SizeForGrid(int unit, double factor) => new GridLength(unit * factor, GridUnitType.Pixel);
 
-        public static Thickness MultiplyForThickness(int unit, double factor)
+        public static Thickness SizeForThickness(int unit, double factor)
             => new Thickness(unit * factor);
-        public static Thickness MultiplyForThickness(int vUnit, double vFactor, int hUnit, double hFactor)
+        public static Thickness SizeForThickness(int vUnit, double vFactor, int hUnit, double hFactor)
             => new Thickness(hUnit * hFactor, vUnit * vFactor, hUnit * hFactor, vUnit * vFactor);
 
-        public static Thickness MultiplyForThickness(int tUnit, double tFactor, int rUnit, double rFactor, int bUnit, double bFactor, int lUnit, double lFactor)
+        public static Thickness SizeForThickness(int tUnit, double tFactor, int rUnit, double rFactor, int bUnit, double bFactor, int lUnit, double lFactor)
             => new Thickness(lUnit * lFactor, tUnit * tFactor, rUnit * rFactor, bUnit * bFactor);
     }
 }
