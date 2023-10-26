b9555219db387fb42545e400869c7c6ed72b8ccd
Hintee <hintea_dan@yahoo.co.uk>
2023-10-26 17:24:08 +0300

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/BindOps.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/BindOps.cs
@@ -1,9 +1,19 @@
-﻿namespace H.Necessaire.RDF.UI.WindowsDesktop
+﻿using Microsoft.UI.Xaml;
+
+namespace H.Necessaire.RDF.UI.WindowsDesktop
 {
     static class BindOps
     {
         public static double Multiply(int unit, double factor) => unit * factor;
 
-        public static Microsoft.UI.Xaml.GridLength MultiplyForGrid(int unit, double factor) => new Microsoft.UI.Xaml.GridLength(unit * factor, Microsoft.UI.Xaml.GridUnitType.Pixel);
+        public static GridLength MultiplyForGrid(int unit, double factor) => new GridLength(unit * factor, GridUnitType.Pixel);
+
+        public static Thickness MultiplyForThickness(int unit, double factor)
+            => new Thickness(unit * factor);
+        public static Thickness MultiplyForThickness(int vUnit, double vFactor, int hUnit, double hFactor)
+            => new Thickness(hUnit * hFactor, vUnit * vFactor, hUnit * hFactor, vUnit * vFactor);
+
+        public static Thickness MultiplyForThickness(int tUnit, double tFactor, int rUnit, double rFactor, int bUnit, double bFactor, int lUnit, double lFactor)
+            => new Thickness(lUnit * lFactor, tUnit * tFactor, rUnit * rFactor, bUnit * bFactor);
     }
 }
