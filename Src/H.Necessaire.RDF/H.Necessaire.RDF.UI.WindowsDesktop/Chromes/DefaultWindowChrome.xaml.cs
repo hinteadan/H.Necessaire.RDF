413f09cc70ca0250dfa38a9e6ee1e60000b4f0b6
Hintee <hintea_dan@yahoo.co.uk>
2023-10-26 12:15:34 +0300

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Chromes/DefaultWindowChrome.xaml.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Chromes/DefaultWindowChrome.xaml.cs
@@ -6,7 +6,7 @@ using System;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace H.Necessaire.RDF.UI.WindowsDesktop
+namespace H.Necessaire.RDF.UI.WindowsDesktop.Chromes
 {
     public sealed partial class DefaultWindowChrome : UserControl
     {
