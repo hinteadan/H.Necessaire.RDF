575a7df2061628596904968ad9f0d43db4942ab5
Hintee <hintea_dan@yahoo.co.uk>
2023-10-30 09:25:08 +0200

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.Runtime/UIComponents/ImAUIComponent.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.Runtime/UIComponents/ImAUIComponent.cs
@@ -5,6 +5,7 @@ namespace H.Necessaire.RDF.UI.Runtime.UIComponents
     public interface ImAUIComponent<TState> where TState : ImAUIComponentState
     {
         bool IsBusy { get; }
+        TState CurrentState { get; }
         Task Initialize();
         Task Destroy();
         Task RunAtStartup();
