cb7363c0dc77f6c70d2454a1eef6dee61d6d3a3b
Hintee <hintea_dan@yahoo.co.uk>
2023-10-31 16:30:25 +0200

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/HomePage.xaml.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/HomePage.xaml.cs
@@ -1,9 +1,11 @@
-using H.Necessaire.RDF.UI.Runtime.UIComponents.Abstracts;
+﻿using H.Necessaire.RDF.UI.Runtime.UIComponents.Abstracts;
 using H.Necessaire.RDF.UI.Runtime.UseCases;
 using H.Necessaire.RDF.UI.WindowsDesktop.Pages.Abstracts;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -49,7 +51,45 @@ namespace H.Necessaire.RDF.UI.WindowsDesktop.Pages
             {
                 State.DebugLabel = DateTime.UtcNow.ToString();
                 await ApplyState(State);
+                await DebugInternetCalls();
             }
+        }
+
+        private async Task DebugInternetCalls()
+        {
+            using (HttpClient http = BuildNewHttpClient())
+            {
+                string httpContent = await http.GetStringAsync("https://hintea.com");
+            }
+        }
+
+        static HttpClient BuildNewHttpClient()
+        {
+            return
+                new HttpClient
+                (
+                    handler: BuildNewSocketsHttpHandler(),
+                    disposeHandler: true
+                );
+        }
+
+        static SocketsHttpHandler BuildNewSocketsHttpHandler()
+        {
+            return
+                new SocketsHttpHandler
+                {
+                    // The maximum idle time for a connection in the pool. When there is no request in
+                    // the provided delay, the connection is released.
+                    // Default value in .NET 6: 1 minute
+                    PooledConnectionIdleTimeout = TimeSpan.FromMinutes(1),
+
+                    // This property defines maximal connection lifetime in the pool regardless
+                    // of whether the connection is idle or active. The connection is reestablished
+                    // periodically to reflect the DNS or other network changes.
+                    // ⚠️ Default value in .NET 6: never
+                    //    Set a timeout to reflect the DNS or other network changes
+                    PooledConnectionLifetime = TimeSpan.FromHours(.5),
+                };
         }
     }
 }
