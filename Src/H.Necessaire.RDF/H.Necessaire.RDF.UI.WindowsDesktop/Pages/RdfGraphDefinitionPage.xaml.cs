64511338f9eca9d4642cc737a49375c6a467a1de
Hintee <hintea_dan@yahoo.co.uk>
2023-10-31 16:08:17 +0200

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/RdfGraphDefinitionPage.xaml.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/RdfGraphDefinitionPage.xaml.cs
@@ -1,19 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using H.Necessaire.RDF.UI.Runtime.UIComponents.Abstracts;
 using H.Necessaire.RDF.UI.WindowsDesktop.Pages.Abstracts;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -36,5 +24,16 @@ namespace H.Necessaire.RDF.UI.WindowsDesktop.Pages
         }
 
         public override string Title => "RDF Graph Definition";
+
+        private async void Back_Click(object sender, RoutedEventArgs e)
+        {
+            using (new ScopedRunner(
+                () => ((Button)sender).IsEnabled = false,
+                () => ((Button)sender).IsEnabled = true)
+            )
+            {
+                await Navi.GoBack();
+            }
+        }
     }
 }
