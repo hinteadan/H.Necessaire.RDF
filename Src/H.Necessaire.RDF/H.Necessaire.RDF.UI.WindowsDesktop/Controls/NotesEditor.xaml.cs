dab3a8c92e1561e1925f964d3ac725754a9e9a83
Hintee <hintea_dan@yahoo.co.uk>
2023-11-01 11:42:27 +0200

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Controls/NotesEditor.xaml.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Controls/NotesEditor.xaml.cs
@@ -1,24 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
+using H.Necessaire.RDF.UI.Runtime.UIComponents.Abstracts;
+using H.Necessaire.RDF.UI.WindowsDesktop.Controls.Abstracts;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System.Collections.Generic;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace H.Necessaire.RDF.UI.WindowsDesktop.Controls
 {
-    public sealed partial class NotesEditor : UserControl
+    public abstract class NotesEditorBase : UserControlBase<NotesEditorBase.ControlState>
+    {
+        public NotesEditorBase() : base(new ControlState { }) { }
+        public class ControlState : UIComponentStateBase
+        {
+            public IList<Note> Notes { get; set; } = new List<Note>();
+        }
+    }
+
+    public sealed partial class NotesEditor : NotesEditorBase
     {
         public NotesEditor()
         {
