e1f02a2d3b91129fd8e96bda38402cf90320ab7f
Hintee <hintea_dan@yahoo.co.uk>
2023-11-01 09:08:48 +0200

    OD: Re-Modelling Take 1 + UI Tools

---
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Controls/Abstracts/UserControlBase.cs
@@ -0,0 +1,105 @@
+﻿using H.Necessaire.RDF.UI.Runtime;
+using H.Necessaire.RDF.UI.Runtime.UIComponents;
+using H.Necessaire.RDF.UI.Runtime.UIComponents.Concrete;
+using H.Necessaire.RDF.UI.Runtime.UINavigation;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.RDF.UI.WindowsDesktop.Controls.Abstracts
+{
+    public abstract partial class UserControlBase<TState> : UserControl, ImAUIComponent<TState>, INotifyPropertyChanged, IAsyncDisposable where TState : ImAUIComponentState
+    {
+        #region Construct
+        readonly UIComponentComposer<TState> composer;
+        readonly ImAUINavigator uiNavigator;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public UserControlBase(TState initialState)
+        {
+            composer = new UIComponentComposer<TState>();
+            composer.ApplyState(initialState);
+            uiNavigator = HNApp.Lication.Deps.Get<ImAUINavigator>();
+
+            Loading += PageUserControlBase_Loading;
+            Loaded += UserControlBase_Loaded;
+        }
+
+        private void PageUserControlBase_Loading(Microsoft.UI.Xaml.FrameworkElement sender, object args)
+        {
+            Initialize().Wait(TimeSpan.FromSeconds(3));
+        }
+
+        private async void UserControlBase_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            using (BusyFlag())
+            {
+                await RunAtStartup();
+            }
+        }
+
+        public virtual async ValueTask DisposeAsync()
+        {
+            Loaded -= UserControlBase_Loaded;
+            Loading -= PageUserControlBase_Loading;
+
+            await Destroy();
+        }
+        #endregion
+
+        public bool IsBusy => composer.IsBusy;
+
+        public RuntimeConfig Config => composer.Config;
+
+        public BrandingStyle Branding => composer.Branding;
+
+        public TState State => composer.State;
+
+        public IDisposable BusyFlag() => composer.BusyFlag();
+        public T Get<T>() => composer.Get<T>();
+
+        public Task<Version> GetAppVersion() => composer.GetAppVersion();
+
+        public ImALogger GetLogger<T>() => composer.GetLogger<T>();
+
+
+
+        public virtual async Task Initialize()
+        {
+            await composer.Initialize();
+        }
+
+        public virtual async Task Destroy()
+        {
+            await composer.Destroy();
+        }
+
+        public virtual async Task OnBusyChanged()
+        {
+            await composer.OnBusyChanged();
+        }
+
+        public virtual async Task RunAtStartup()
+        {
+            await composer.RunAtStartup();
+        }
+
+        public virtual async Task ApplyState(TState state)
+        {
+            await composer.ApplyState(state);
+            NotifyStateChanged();
+        }
+
+        protected ImAUINavigator Navi => uiNavigator;
+
+        protected void NotifyStateChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+    }
+}
--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/Abstracts/PageUserControlBase.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Pages/Abstracts/PageUserControlBase.cs
@@ -2,6 +2,7 @@
 using H.Necessaire.RDF.UI.Runtime.UIComponents;
 using H.Necessaire.RDF.UI.Runtime.UIComponents.Concrete;
 using H.Necessaire.RDF.UI.Runtime.UINavigation;
+using H.Necessaire.RDF.UI.WindowsDesktop.Controls.Abstracts;
 using Microsoft.UI.Xaml.Controls;
 using System;
 using System.ComponentModel;
@@ -9,101 +10,24 @@ using System.Threading.Tasks;
 
 namespace H.Necessaire.RDF.UI.WindowsDesktop.Pages.Abstracts
 {
-    public abstract partial class PageUserControlBase<TState> : UserControl, ImAUIPage<TState>, INotifyPropertyChanged, IAsyncDisposable where TState : ImAUIComponentState
+    public abstract partial class PageUserControlBase<TState> : UserControlBase<TState>, ImAUIPage<TState>, INotifyPropertyChanged, IAsyncDisposable where TState : ImAUIComponentState
     {
         #region Construct
-        readonly UIPageComposer<TState> composer;
-        readonly ImAUINavigator uiNavigator;
+        readonly UIPageComposer<TState> pageComposer;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public PageUserControlBase(TState initialState)
+        public PageUserControlBase(TState initialState) : base(initialState)
         {
-            composer = new UIPageComposer<TState>(GetType());
-            composer.ApplyState(initialState);
-            uiNavigator = HNApp.Lication.Deps.Get<ImAUINavigator>();
-
-            Loading += PageUserControlBase_Loading;
-            Loaded += PageUserControlBase_Loaded;
-        }
-
-        private void PageUserControlBase_Loading(Microsoft.UI.Xaml.FrameworkElement sender, object args)
-        {
-            Initialize().Wait(TimeSpan.FromSeconds(3));
-        }
-
-        private async void PageUserControlBase_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            using (BusyFlag())
-            {
-                await RunAtStartup();
-            }
-        }
-
-        public async ValueTask DisposeAsync()
-        {
-            Loaded -= PageUserControlBase_Loaded;
-            Loading -= PageUserControlBase_Loading;
-
-            await Destroy();
+            pageComposer = new UIPageComposer<TState>(GetType());
+            pageComposer.ApplyState(initialState);
         }
         #endregion
 
-        public virtual string Title => composer.Title;
+        public virtual string Title => pageComposer.Title;
 
-        public bool IsBusy => composer.IsBusy;
-
-        public RuntimeConfig Config => composer.Config;
-
-        public BrandingStyle Branding => composer.Branding;
-
-        public TState State => composer.State;
-
-        public IDisposable BusyFlag() => composer.BusyFlag();
-        public T Get<T>() => composer.Get<T>();
-
-        public Task<Version> GetAppVersion() => composer.GetAppVersion();
-
-        public ImALogger GetLogger<T>() => composer.GetLogger<T>();
-
-
-
-        public virtual async Task Initialize()
-        {
-            await composer.Initialize();
-        }
-
-        public virtual async Task Destroy()
-        {
-            await composer.Destroy();
-        }
-
-        public virtual async Task OnBusyChanged()
-        {
-            await composer.OnBusyChanged();
-        }
-
-        public virtual async Task RunAtStartup()
-        {
-            await composer.RunAtStartup();
-        }
 
         public virtual async Task Use(UINavigationParams navigationParams)
         {
-            await composer.Use(navigationParams);
-        }
-
-        public async Task ApplyState(TState state)
-        {
-            await composer.ApplyState(state);
-            NotifyStateChanged();
-        }
-
-        protected ImAUINavigator Navi => uiNavigator;
-
-        private void NotifyStateChanged()
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+            await pageComposer.Use(navigationParams);
         }
     }
 }
