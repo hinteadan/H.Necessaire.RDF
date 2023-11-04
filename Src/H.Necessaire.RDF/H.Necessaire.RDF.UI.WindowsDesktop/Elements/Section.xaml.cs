9ffbd22207c6c0507fc97068307d4fd02703a29a
Hintee <hintea_dan@yahoo.co.uk>
2023-11-04 11:00:12 +0200

    OD: Re-Modelling Take 1 + UI Tools

--- Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Elements/Section.xaml.cs
+++ Src/H.Necessaire.RDF/H.Necessaire.RDF.UI.WindowsDesktop/Elements/Section.xaml.cs
@@ -1,7 +1,5 @@
-using H.Necessaire.RDF.UI.WindowsDesktop.Chromes;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Windows.UI.Xaml.Markup;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,32 +8,14 @@ namespace H.Necessaire.RDF.UI.WindowsDesktop.Elements
 {
     public sealed partial class Section : UserControl
     {
-        static readonly DependencyProperty title =
-            DependencyProperty.Register(nameof(Title), typeof(string), typeof(Section), new PropertyMetadata(null));
-        static readonly DependencyProperty titleVisibility =
-            DependencyProperty.Register(nameof(TitleVisibility), typeof(Visibility), typeof(Section), new PropertyMetadata(null));
-        static readonly DependencyProperty body =
-            DependencyProperty.Register(nameof(Body), typeof(object), typeof(Section), new PropertyMetadata(null));
-
         public Section()
         {
             this.InitializeComponent();
         }
 
-        public string Title {
-            get => GetValue(title) as string;
-            set { 
-                SetValue(title, value);
-                TitleVisibility = value.IsEmpty() ? Visibility.Collapsed : Visibility.Visible;
-            }
-        }
-        public Visibility TitleVisibility { get => (Visibility)GetValue(titleVisibility); set => SetValue(titleVisibility, value); }
+        public string Title { get; set; }
+        public Visibility TitleVisibility { get; set; }
         public string Description { get; set; }
-
-        public object Body
-        {
-            get => GetValue(body);
-            set => SetValue(body, value);
-        }
+        public UIElement Body { get; set; }
     }
 }
